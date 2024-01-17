30800edfc120ec0a34abf5278e04b431ed30b534
unknown <roshan.r2022@vitstudent.ac.in>
2024-01-17 10:41:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function handlesubmit() {
+  e.preventDefault();
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  alert(email);
+  alert(password);
+}
